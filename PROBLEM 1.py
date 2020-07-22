08b908dc4c16dd4830c6a57509c75c27360df05a
PanitaVaishnavi <68539219+PanitaVaishnavi@users.noreply.github.com>
2020-07-22 19:29:28 +0530

    Add files via upload

---
+++ PROBLEM 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#sum of n numbers using while loop
+n=int(input("enter n value:"))
+if n<0:
+    print("Enter a positive number")
+else:
+    sum=0
+    while(n>0):
+        sum+=n
+        n-=1
+    print(sum)
+
+
+# In[ ]:
+
+
+
+
